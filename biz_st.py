82b9976b263dfc610aff4d376ca7d8d55e8ef51e
gooddaysop-ctrl <gooddaysop@gmail.com>
2026-05-26 15:16:38 +0900

    Add files via upload

--- biz_st.py
+++ biz_st.py
@@ -2,4 +2,15 @@ import streamlit as st
 # as st는 python에 streamlit을 가져올 때, 이름을 축약한 것.
 st.title('첫번재 웹 어플 만들기 👋 📺')
 
-"## 이건 부제목"
+"# 첫번재 웹 어플 만들기 👋 📺"
+'''### Magic 적용
+ 1. ordered item
+
+
+ -강조: **unordered item**
+ -기울임: *unordered item*
+ 2. ordered item
+ 3. ordered item
+'''
+
+# streamlit이 .py를 웹용 프로그램으로 바꾸ㅓ줌.(나는 깃허브에 .py 입력)
